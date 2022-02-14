61522e949d0dc485932ee7041fb1bb8947f9ec09
windcalm666 <79242740+windcalm666@users.noreply.github.com>
2022-02-14 10:05:52 +0900

    Initial commit

---
+++ src/main/java/com/example/SkillCheckAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillCheckAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillCheckAppApplication.class, args);
+	}
+
+}
